1ec253cf64a8e3809474e49a7840aa44ad7775cf
AyusAgarwal <99806027+AyusAgarwal@users.noreply.github.com>
2023-04-24 20:17:32 +0530

    Add files via upload

---
+++ sweet.js
@@ -0,0 +1,25 @@
+console.log('Hi');
+let cnt=0;
+let k=document.getElementsByClassName('bt');
+document.getElementById("car").style.width='0';
+document.getElementById("bt1").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+document.getElementById("bt2").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+document.getElementById("bt3").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+document.getElementById("bt4").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+document.getElementById("bt5").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+document.getElementById("bt6").addEventListener("click",()=>{
+    document.getElementById("car").style.width='40px';
+})
+
+// let x=document.getElementById('f');
+// x.innerHTML.style.color='black';
